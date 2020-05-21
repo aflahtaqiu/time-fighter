e07c16103a7e63aeabeffe6aa08777ec2ccc677d
aflahtaqiu <aflahts98@gmail.com>
2020-05-21 22:09:19 +0700

    produce todo list

--- app/src/main/java/id/aflah/timefighter/MainActivity.kt
+++ app/src/main/java/id/aflah/timefighter/MainActivity.kt
@@ -4,11 +4,135 @@ package id.aflah.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var score = 0
+
+    private var isGameStarted = false
+
+    private lateinit var countDownTimer: CountDownTimer
+
+    private val initCountDownTime = 10_000L
+    private val countDownInterval = 1_000L
+    private var timeLeft = 10_000L
+
+    companion object {
+        private val SCORE_KEY = "SCORE"
+        private val TIME_LEFT_KEY = "TIME_LEFT"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnTapMe.setOnClickListener {
+            incrementScore()
+        }
+
+        tvGameScore.text = "Your score: $score"
+
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(SCORE_KEY)
+            timeLeft = savedInstanceState.getLong(TIME_LEFT_KEY)
+            restoreGame()
+        } else {
+            resetGame()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY, score)
+        outState.putLong(TIME_LEFT_KEY, timeLeft)
+        countDownTimer.cancel()
+    }
+
+    private fun incrementScore () {
+        if (!isGameStarted) {
+            countDownTimer.start()
+            isGameStarted = true
+        }
+
+        score++
+
+        val yourNewScore = "Your New Score is $score"
+        tvGameScore.text = yourNewScore
+    }
+
+    private fun endedGame () {
+        Toast.makeText(this, "Time is up, your score is $score", Toast.LENGTH_SHORT).show()
+        resetGame()
+    }
+
+    private fun resetGame() {
+        score = 0
+        tvGameScore.text = "Your score is $score"
+
+        val initTimeLeft = initCountDownTime / 1000
+        tvTimeLeft.text = "Time left: $initTimeLeft"
+
+        countDownTimer = object : CountDownTimer(initCountDownTime, countDownInterval) {
+            override fun onFinish() {
+                endedGame()
+            }
+
+            override fun onTick(p0: Long) {
+                timeLeft = p0
+                val timeleft = p0 / 1000
+                tvTimeLeft.text = "Time left: $timeleft"
+            }
+        }
+
+        isGameStarted = false
+    }
+
+    private fun restoreGame () {
+        tvGameScore.text = "Your score: $score"
+
+        val restoredTime = timeLeft / 1000
+        tvTimeLeft.text = "Time left: $restoredTime"
+
+        countDownTimer = object : CountDownTimer(initCountDownTime, countDownInterval) {
+            override fun onFinish() {
+                endedGame()
+            }
+
+            override fun onTick(p0: Long) {
+                timeLeft = p0
+                val timeleft = p0 / 1000
+                tvTimeLeft.text = "Time left: $timeleft"
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.actionbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        if (item.itemId == R.id.menu_info) {
+            showInfoMessage("Time Fighter App", "Created by Aflahtaqiu")
+        }
+        return true
+    }
+
+    private fun showInfoMessage (title:String, message:String) {
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.create().show()
     }
 }
